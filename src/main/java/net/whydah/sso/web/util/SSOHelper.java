a56beee19e8e7737dc222a062cbb714ba06563eb
totto <totto@totto.org>
2019-04-03 18:53:25 +0200

    Upgraded to Apache HTTPClient 4.x (from 3.1)

--- src/main/java/net/whydah/sso/web/util/SSOHelper.java
+++ src/main/java/net/whydah/sso/web/util/SSOHelper.java
@@ -4,8 +4,13 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
 import net.whydah.sso.config.AppConfig;
 import net.whydah.sso.web.data.ApplicationCredential;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,12 +19,13 @@ import javax.ws.rs.core.UriBuilder;
 import java.io.IOException;
 import java.net.ConnectException;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SSOHelper {
     private static final Logger log = LoggerFactory.getLogger(SSOHelper.class);
     final URI BASE_URI;
     private WebResource tokenServiceResource;
-    private final HttpClient httpClient;
     private String applicationid;
     private String applicationname;
     private String applicationsecret;
@@ -27,11 +33,11 @@ public class SSOHelper {
 
     public SSOHelper(String tokenServiceUri) {
 
-        SSLTool.disableCertificateValidation();
+//        SSLTool.disableCertificateValidation();
         BASE_URI = UriBuilder.fromUri(tokenServiceUri).build();
         Client c = Client.create();
         tokenServiceResource = c.resource(BASE_URI);
-        httpClient = new HttpClient();
+        HttpClient httpClient = new DefaultHttpClient();
         try {
             applicationname = AppConfig.readProperties().getProperty("applicationname");
             applicationid = AppConfig.readProperties().getProperty("applicationid");
@@ -48,23 +54,26 @@ public class SSOHelper {
 
     public String logonApplication() {
         WebResource logonResource = tokenServiceResource.path("/logon");
-        PostMethod postMethod = new PostMethod(logonResource.toString());
-        postMethod.addParameter(RequestHelper.APPLICATIONCREDENTIAL, applicationCredential.toXML());
-
         String targetUrl = logonResource.getURI().toString();
         try {
-            int responseCode = httpClient.executeMethod(postMethod);
-            String responseAsString = postMethod.getResponseBodyAsString();
+            List<NameValuePair> postParameters = new ArrayList<>();
+            postParameters.add(new BasicNameValuePair(RequestHelper.APPLICATIONCREDENTIAL, applicationCredential.toXML()));
+            URIBuilder uriBuilder = new URIBuilder(logonResource.toString());
+            HttpPost postRequest = new HttpPost(uriBuilder.build());
+//            postRequest.setHeader("Content-Type", "application/json");
+            HttpClient client = new DefaultHttpClient();
+            HttpResponse httpResponse = client.execute(postRequest);
+            int responseCode = httpResponse.getStatusLine().getStatusCode();
+            String responseAsString = httpResponse.getEntity().getContent().toString();
             log.trace("ResponseCode={} when executing {}, ApplicationToken: \n {}  ApplicationCredential: {}", responseCode, targetUrl, responseAsString, applicationCredential.toXML());
             return responseAsString;
         } catch (ConnectException ce) {
             log.error("logonApplication failed. targetUrl={}, ConnectException: {}", targetUrl, ce.getMessage());
         } catch (SSLHandshakeException sslE) {
             log.error("logonApplication failed. targetUrl={}, SSLHandshakeException: {}", targetUrl, sslE.getMessage());
-        } catch (IOException ioe) {
+        } catch (Exception ioe) {
             log.error("logonApplication failed. targetUrl={}", targetUrl, ioe);
         } finally {
-            postMethod.releaseConnection();
         }
         return null;
     }
@@ -78,19 +87,24 @@ public class SSOHelper {
         }
         String applicationTokenId = XpathHelper.getAppTokenIdFromAppToken(appTokenXML);
         String path = tokenServiceResource.path("user/").toString() + applicationTokenId + "/get_usertoken_by_userticket"; // webResource.path("/iam/")
-        PostMethod postMethod = new PostMethod(path);
-        postMethod.addParameter(RequestHelper.APPTOKEN, appTokenXML);
-        postMethod.addParameter(RequestHelper.USERTICKET, userticket);
+
+
         log.trace("Executing get_usertoken_by_userticket, path={}, appToken={}, userticket={}", path, appTokenXML, userticket);
         try {
-            int responseCode = httpClient.executeMethod(postMethod);
-            String userTokenXml = postMethod.getResponseBodyAsString();
+            List<NameValuePair> postParameters = new ArrayList<>();
+            postParameters.add(new BasicNameValuePair(RequestHelper.APPTOKEN, appTokenXML));
+            postParameters.add(new BasicNameValuePair(RequestHelper.USERTICKET, userticket));
+            URIBuilder uriBuilder = new URIBuilder(path);
+            HttpPost postRequest = new HttpPost(uriBuilder.build());
+            HttpClient client = new DefaultHttpClient();
+            HttpResponse httpResponse = client.execute(postRequest);
+            int responseCode = httpResponse.getStatusLine().getStatusCode();
+            String userTokenXml = httpResponse.getEntity().getContent().toString();
             log.trace("Executed get_usertoken_by_userticket, responseCode={}, userToken=\n{}", responseCode, userTokenXml);
             return userTokenXml;
-        } catch (IOException ioe) {
+        } catch (Exception ioe) {
             log.error("get_usertoken_by_userticket failed", ioe);
         } finally {
-            postMethod.releaseConnection();
         }
         return null;
     }
@@ -106,19 +120,22 @@ public class SSOHelper {
 
         String applicationTokenId = XpathHelper.getAppTokenIdFromAppToken(appTokenXML);
         String path = tokenServiceResource.path("user/").toString() + applicationTokenId + "/get_usertoken_by_usertokenid"; // webResource.path("/iam/")
-        PostMethod postMethod = new PostMethod(path);
-        postMethod.addParameter(RequestHelper.APPTOKEN, appTokenXML);
-        postMethod.addParameter(RequestHelper.USERTOKENID, usertokenID);
         log.trace("Executing get_usertoken_by_usertokenid, path={}, appToken={}, usertokenid={}", path, appTokenXML, usertokenID);
         try {
-            int responseCode = httpClient.executeMethod(postMethod);
-            String userTokenXml = postMethod.getResponseBodyAsString();
+            List<NameValuePair> postParameters = new ArrayList<>();
+            postParameters.add(new BasicNameValuePair(RequestHelper.APPTOKEN, appTokenXML));
+            postParameters.add(new BasicNameValuePair(RequestHelper.USERTOKENID, usertokenID));
+            URIBuilder uriBuilder = new URIBuilder(path);
+            HttpPost postRequest = new HttpPost(uriBuilder.build());
+            HttpClient client = new DefaultHttpClient();
+            HttpResponse httpResponse = client.execute(postRequest);
+            int responseCode = httpResponse.getStatusLine().getStatusCode();
+            String userTokenXml = httpResponse.getEntity().getContent().toString();
             log.trace("Executed get_usertoken_by_usertokenid, responseCode={}, userToken=\n{}", responseCode, userTokenXml);
             return userTokenXml;
-        } catch (IOException ioe) {
+        } catch (Exception ioe) {
             log.error("get_usertoken_by_usertokenid failed", ioe);
         } finally {
-            postMethod.releaseConnection();
         }
         return null;
     }
