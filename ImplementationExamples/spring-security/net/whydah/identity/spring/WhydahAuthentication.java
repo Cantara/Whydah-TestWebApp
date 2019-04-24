850c3e51789f80db88bcb5893d9fbb14eda3de55
totto <totto@totto.org>
2019-04-24 08:53:48 +0200

    some cleanup

---
+++ ImplementationExamples/spring-security/net/whydah/identity/spring/WhydahAuthentication.java
@@ -0,0 +1,70 @@
+package net.whydah.identity.spring;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+/**
+ * Loosely based upon code from Gunnar Skjold (Origin AS)
+ *
+ * @author Gunnar Skjold
+ * @author <a href="bard.lind@gmail.com">Bard Lind</a>
+ */
+public class WhydahAuthentication implements Authentication {
+    private final String username;
+    private final User user;
+    private final WebAuthenticationDetails details;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    private boolean authenticated;
+
+    public WhydahAuthentication(String username, User user, WebAuthenticationDetails details) {
+        this.username = username;
+        this.user = user;
+        this.details = details;
+        this.authorities = new HashSet<GrantedAuthority>(user.getAuthorities());
+        authenticated = true;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getDetails() {
+        return details;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return user;
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return authenticated;
+    }
+
+    @Override
+    public void setAuthenticated(boolean authenticated) throws IllegalArgumentException {
+        this.authenticated = authenticated;
+    }
+
+    @Override
+    public String getName() {
+        return username;
+    }
+}
---
+++ ImplementationExamples/spring-security/net/whydah/identity/spring/WhydahRole.java
@@ -0,0 +1,41 @@
+package net.whydah.identity.spring;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Loosely based upon code from Gunnar Skjold (Origin AS)
+ *
+ * @author Gunnar Skjold
+ * @author <a href="bard.lind@gmail.com">Bard Lind</a>
+ */
+@XmlRootElement(name = "role")
+public class WhydahRole {
+    private String name, value;
+
+    public WhydahRole() {
+    }
+
+    public WhydahRole(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    @XmlAttribute
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @XmlAttribute
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
